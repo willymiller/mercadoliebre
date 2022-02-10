04201c681a865c80687dfbe125da2eb1b91c41d7
willymiller <65316037+willymiller@users.noreply.github.com>
2022-02-10 15:18:24 -0300

    Sin styles externos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.listen(process.env.port || 3000, function() {
   console.log("Servidor corriendo");

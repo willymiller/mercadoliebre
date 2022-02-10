3e9c8a62947ac7ab56c48e272020b0562178b628
willymiller <65316037+willymiller@users.noreply.github.com>
2022-02-10 15:02:44 -0300

    Proyecto sin normalize.css

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ app.listen(process.env.port || 3000, function() {
   console.log("Servidor corriendo");
 });
 
-/* app.listen(3030, () => {
-  console.log("Servidor corriendo");
-}); */
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
